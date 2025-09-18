76760707d949fdfb26771e93d874927eae6a3fbd
progikusok <moyomylo11@gmail.com>
2025-09-18 16:21:52 +0300

    feat: Добавлен rrweb

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,33 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import * as rrweb from 'rrweb';
 import "./index.css";
 
+
 createRoot(document.getElementById("root")!).render(<App />);
+
+let events = [];
+
+rrweb.record({
+  emit(event) {
+    // push event into the events array
+    events.push(event);
+  },
+});
+
+// this function will send events to the backend and reset the events array
+function save() {
+  const body = JSON.stringify({ events });
+  events = [];
+//   fetch('http://YOUR_BACKEND_API', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body,
+//   });
+    console.log(body);
+}
+
+// save events every 10 seconds
+setInterval(save, 10 * 1000);
